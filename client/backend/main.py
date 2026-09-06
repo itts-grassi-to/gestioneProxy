def3b43c184c791213854e328fbf2ed7ccf16b77
Daniele Ortu <daniele.ortu@itisgrassi.edu.it>
2026-09-06 17:27:11 +0200

    Messa in produzione

--- client/backend/main.py
+++ client/backend/main.py
@@ -39,6 +39,7 @@ app.include_router(functionality_router)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        "http://192.168.3.231:3010",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "http://localhost:3000",
